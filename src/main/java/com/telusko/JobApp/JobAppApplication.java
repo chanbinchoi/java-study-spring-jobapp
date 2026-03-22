4f79c496a593240a53b0628c1a005d80066424e5
chanbinchoi <classy9611@gmail.com>
2026-03-22 13:04:50 +0900

    JobApp end

--- src/main/java/com/telusko/JobApp/JobAppApplication.java
+++ src/main/java/com/telusko/JobApp/JobAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JopAppApplication {
+public class JobAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JopAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JobAppApplication.class, args);
+    }
 
 }
